f024128b24cbb8b37d12322c2db66937165261d7
Stephen Hill <stephen.hill9@nhs.net>
2020-06-10 11:54:06 +0100

    Guidance pages

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Get help to buy food and milk (Healthy Start)',
 
   // Default port that prototype runs on
   port: '3000',
